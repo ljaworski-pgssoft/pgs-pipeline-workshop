7b188da4d8f244c94d0b411aa3986d52579dc614
ljaworski-pgssoft <ljaworski@pgs-soft.com>
2018-01-24 08:52:51 +0100

    Update start_server.sh
    updating start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "starting the app"
-nohup java -jar /opt/workshop-app/workshop-1.0.jar > /opt/workshop-app/app.log 2>&1
+nohup java -jar /opt/workshop-app/workshop-1.0.jar > /opt/workshop-app/app.log 2>&1 &
